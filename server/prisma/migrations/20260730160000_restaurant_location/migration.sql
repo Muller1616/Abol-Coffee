54f7818ef05428b0aaae8aa964b55f838a1466fd
Muller16 <mulugetaabeje16@gmail.com>
2026-07-30 16:49:20 +0300

    Add migration for restaurant location fields
    - Created a new SQL migration to add city, state, country, postalCode, latitude, and longitude columns to the restaurants table, enhancing location data management in the database.

---
+++ server/prisma/migrations/20260730160000_restaurant_location/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "restaurants" ADD COLUMN     "city" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "postalCode" TEXT,
+ADD COLUMN     "latitude" DOUBLE PRECISION,
+ADD COLUMN     "longitude" DOUBLE PRECISION;
